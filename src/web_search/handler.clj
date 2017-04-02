ff07bc26722f1550e549303405540daed6ef138a
Andrew <brownmoose3q@gmail.com>
2017-04-02 08:48:59 +0300

    Stripped unexpexted trash code commitment

--- src/web_search/handler.clj
+++ src/web_search/handler.clj
@@ -3,10 +3,7 @@
             [compojure.route :as route]
             [web-search.search :as search]
             [ring.middleware.params :as rparams]
-
-            [ring.middleware.json :as rjson ]
-            [ring.util.response :refer [response]])
-  )
+            ))
 
 (defroutes routes
   (GET "/search" {{:strs [query plimit]} :query-params}
@@ -14,11 +11,8 @@
          (let [pretty-html (->> (map (fn [[k v]] (format "<li>%s : %s</li>" k v)) results)
                                 (reduce str)
                                 )]
-           (-> (response (format  "<h1>results:<ul>%s</ul></h1>" pretty-html))
-               (header "json-result" results)))))
+           (format  "<h1>results:<ul>%s</ul></h1>" pretty-html))))
   (route/not-found "<h1>Nothing to look here :P</h1>"))
 
 (def app (-> #'routes
-             rparams/wrap-params
-             rjson/wrap-json-response
-             ))
+             rparams/wrap-params))
