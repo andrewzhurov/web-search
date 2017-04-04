ea89aa7ab0486de206c61e1c3d8a50872ed0254f
Andrew <brownmoose3q@gmail.com>
2017-04-04 16:45:31 +0300

    Critical bug fix. Parallel limit now DOES count; unnecessary code stripped

--- src/web_search/handler.clj
+++ src/web_search/handler.clj
@@ -7,11 +7,11 @@
 
 (defroutes routes
   (GET "/search" {{:strs [query plimit]} :query-params}
-       (let [results (doall (search/parallel-domen-statistic query 1))]
-         (let [pretty-html (->> (map (fn [[k v]] (format "<li>%s : %s</li>" k v)) results)
-                                (reduce str)
-                                )]
-           (format  "<h1>results:<ul>%s</ul></h1>" pretty-html))))
+       (let [results (doall (search/parallel-domen-statistic query plimit))
+             pretty-html (->> (map (fn [[k v]] (format "<li>%s : %s</li>" k v)) results)
+                              (reduce str)
+                              )]
+         (format  "<h1>results:<ul>%s</ul></h1>" pretty-html)))
   (route/not-found "<h1>Nothing to look here :P</h1>"))
 
 (def app (-> #'routes
